12140c6fb0b6a9ea51db478f26310edd9d6aed98
Tamas K. Papp <tkpapp@gmail.com>
2017-10-12 14:32:17 +0200

    Initial commit.

--- src/RaggedData.jl
+++ src/RaggedData.jl
@@ -1,5 +1,76 @@
 module RaggedData
 
-# package code goes here
+using Parameters
+using Lazy
+
+import Base: length, size, indices, push!, count, sizehint!, getindex, eltype
+
+export RaggedCounter, RaggedCollate, next_index!, RaggedIndex, collate_index_keys
+
+mutable struct RaggedCount{S <: Integer}
+    index::S
+    count::S
+end
+
+struct RaggedCounter{T,S}
+    dict::Dict{T,RaggedCount{S}}
+end
+
+RaggedCounter(T,S::Type{<:Integer}) =
+    RaggedCounter{T,S}(Dict{T,RaggedCount{S}}())
+
+function push!(rc::RaggedCounter{T,S}, key) where {T,S}
+    @unpack dict = rc
+    if haskey(dict, key)
+        dict[key].count += one(S)
+    else
+        dict[key] = RaggedCount(S(length(dict)) + one(S), one(S))
+    end
+    nothing
+end
+
+count(rc::RaggedCounter) = sum(v.count for v in values(rc.dict))
+
+@forward RaggedCounter.dict length, sizehint!
+
+struct RaggedCollate{T,S}
+    dict::Dict{T,S}
+    total::S
+end
+
+next_index!(coll::RaggedCollate{T,S}, key) where {T,S} = coll.dict[key] += one(S)
+
+struct RaggedIndex{S}
+    cumsum::Vector{S}
+end
+
+@forward RaggedIndex.cumsum length, size, indices
+
+function getindex(ri::RaggedIndex{S}, i) where S
+    UnitRange{S}(i == 1 ? one(S) : (ri.cumsum[i-1]+one(S)), ri.cumsum[i])
+end
+
+eltype(ri::RaggedIndex{S}) where S = UnitRange{S}
+
+function collate_index_keys(rc::RaggedCounter{T,S},
+                            collect_keys = false) where {T,S}
+    kv = collect(rc.dict)
+    sort!(kv, by = x -> x.second.index)
+    partial_sum = zero(S)
+    dict = Dict{T, S}()
+    sizehint!(dict) = length(kv)
+    index = similar(kv, S)
+    keys = ifelse(collect_keys, similar(kv, T), Vector{T}(0))
+    @inbounds for i in indices(kv, 1)
+        key = kv[i].first
+        dict[key] = partial_sum
+        partial_sum += kv[i].second.count
+        index[i] = partial_sum
+        if collect_keys
+            keys[i] = key
+        end
+    end
+    RaggedCollate(dict, partial_sum), RaggedIndex(index), keys
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,46 @@
 using RaggedData
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+@testset "ragged data processing" begin
+    # initialize and countr
+    rc = RaggedCounter(Int,Int32)
+    push!(rc, 3)
+    push!(rc, 2)
+    push!(rc, 1)
+    for _ in 1:3
+        push!(rc, 1)
+    end
+    for _ in 1:10
+        push!(rc, 2)
+    end
+    for _ in 1:7
+        push!(rc, 3)
+    end
+    @test length(rc) == 3
+    @test count(rc) == 3+3+10+7
+    # test index and collation
+    coll, ix, ks = collate_index_keys(rc, true)
+    @test ks == Int32[3, 2, 1]
+    # index
+    @test length(ix) ≡ 3
+    @test size(ix) ≡ (3,)
+    @test eltype(ix) ≡ UnitRange{Int32}
+    @test ix[1] ≡ UnitRange{Int32}(1, 8)
+    @test ix[2] ≡ UnitRange{Int32}(9, 19)
+    @test ix[3] ≡ UnitRange{Int32}(20, 23)
+    @test indices(ix) ≡ indices(ones(3))
+    # collation
+    @test next_index!(coll, 3) == 1
+    for i in 9:15
+        @test next_index!(coll, 2) == i
+    end
+    for i in 20:23
+        @test next_index!(coll, 1) == i
+    end
+    for i in 16:19
+        @test next_index!(coll, 2) == i
+    end
+    for i in 2:8
+        @test next_index!(coll, 3) == i
+    end
+end
