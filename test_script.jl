71f68e73144c0b29b65f8af03043d7ea5de6c5f8
Tamas K. Papp <tkpapp@gmail.com>
2017-10-12 14:41:04 +0200

    Moved test code to script file, using master Lazy, checking out isinferred.

---
+++ test_script.jl
@@ -0,0 +1,5 @@
+Pkg.clone(pwd())
+Pkg.checkout("Lazy")
+Pkg.clone("https://github.com/afniedermayer/InferenceUtilities.jl.git") # until registered
+Pkg.build("RaggedData")
+Pkg.test("RaggedData"; coverage=true)
