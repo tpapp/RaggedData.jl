53307be028008fd985540cea0e10d19b33fbd255
Tamas K. Papp <tkpapp@gmail.com>
2017-10-14 12:37:34 +0200

    Disabled testing on nightly until Lazy.jl is fixed.
    See https://github.com/MikeInnes/Lazy.jl/issues/70 .

--- test_script.jl
+++ test_script.jl
@@ -1,5 +1,5 @@
 Pkg.clone(pwd())
-Pkg.checkout("Lazy")
+# Pkg.checkout("Lazy")
 Pkg.clone("https://github.com/afniedermayer/InferenceUtilities.jl.git") # until registered
 Pkg.build("RaggedData")
 Pkg.test("RaggedData"; coverage=true)
