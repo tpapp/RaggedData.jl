2727f6f606ea456fed66e22fc42fdf47effc1674
Tamas K. Papp <tkpapp@gmail.com>
2017-10-21 16:11:42 +0200

    Added docstrings.

--- src/RaggedData.jl
+++ src/RaggedData.jl
@@ -8,11 +8,34 @@ import Base: length, size, indices, push!, count, sizehint!, getindex, eltype
 
 export RaggedCounter, RaggedCollate, next_index!, RaggedIndex, collate_index_keys
 
+"""
+    RaggedCount(index, count)
+
+An element in a `RaggedCounter`. `index` allows ordering of the elements as they
+are encountered, `count` keeps track of the number of elements so far in the
+data for a particular key.
+"""
 @auto_hash_equals mutable struct RaggedCount{S <: Integer}
     index::S
     count::S
 end
 
+"""
+    RaggedCounter(T, S)
+
+Count keys of type `T` with integers of type `S`. New keys can be added with
+`push!(rc::RaggedCounter, key)`. The order of new keys is preserved.
+
+`count(::RaggedCounter)` returns the total number of keys counter, while
+`length(::RaggedCounter)` the number of unique keys. Use `sizehint!` for a
+slight speed increase if you can estimate the number of elements in advance.
+
+## Recommended usage
+
+Use in the *first pass* of ingesting the data to count the number each key
+occurs. Then generate a collator and indexer using `collate_index_keys` for the
+*second pass* and then indexing.
+"""
 @auto_hash_equals struct RaggedCounter{T,S}
     dict::Dict{T,RaggedCount{S}}
 end
@@ -34,6 +57,13 @@ count(rc::RaggedCounter) = sum(v.count for v in values(rc.dict))
 
 @forward RaggedCounter.dict length, sizehint!
 
+"""
+    RaggedCollate
+
+Use in the *second pass* of reading the data to return increasing indices
+(`nextindex!`) that form an ordered partition of `1:count` after the whole data
+is traversed.
+"""
 @auto_hash_equals struct RaggedCollate{T,S}
     dict::Dict{T,S}
     total::S
@@ -41,17 +71,57 @@ end
 
 next_index!(coll::RaggedCollate{T,S}, key) where {T,S} = coll.dict[key] += one(S)
 
+"""
+    RaggedIndex(count_cumsum)
+
+Create an index that maps key indices (with given counts) to indices of a flat
+vector. See `collate_index_keys` for efficient creation from `RaggedCounter`s.
+
+Example:
+```jldoctest
+julia> ri = RaggedIndex(cumsum(1:3))
+RaggedData.RaggedIndex{Int64}([1, 3, 6])
+
+julia> ri[1]
+1:1
+
+julia> ri[2]
+2:3
+
+julia> ri[3]
+4:6
+```
+"""
 @auto_hash_equals struct RaggedIndex{S}
     cumsum::Vector{S}
 end
 
 @forward RaggedIndex.cumsum length, size, indices
 
+count(ri::RaggedIndex) = ri.cumsum[end]
+
 function getindex(ri::RaggedIndex{S}, i) where S
-    UnitRange{S}(i == 1 ? one(S) : (ri.cumsum[i-1]+one(S)), ri.cumsum[i])
+    UnitRange{S}(one(S) + (i == 1 ? zero(eltype(cumsum)) : cumsum[i-1]),
+                 ri.cumsum[i])
 end
 
 eltype(ri::RaggedIndex{S}) where S = UnitRange{S}
+
+"""
+
+"""
+function _subset(ri::RaggedIndex{S}, I) where {S}
+    partial_sum = zero(S)
+    sub_cumsum = similar(Array{S}, indices(ix))
+    sub_I = Vector{S}()
+    for (j,i) in enumerate(I)
+        ix = ri[i]
+        append!(sub_I, ix)
+        partial_sum += length(ix)
+        sub_cumsum[j] = partial_sum
+    end
+    RaggedIndex(sub_cumsum), sub_I
+end
 
 function collate_index_keys(rc::RaggedCounter{T,S},
                             collect_keys = false) where {T,S}
