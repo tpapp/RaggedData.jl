aa40649bb218c46cc9e1a034224c5448be127cfd
Tamas K. Papp <tkpapp@gmail.com>
2017-10-25 14:01:40 +0200

    Added a testcase for non-integer indices.

--- test/runtests.jl
+++ test/runtests.jl
@@ -113,4 +113,5 @@ end
     @test_throws MethodError RaggedColumn(rc, 1:1)
     @test collect(rc2) ==
         Any[[1, 2, 3, 4, 5], [6, 7, 8], [9, 10], [11, 12, 13, 14, 15, 16, 17]]
+    @test rc2[2:3] == [[6, 7, 8], [9, 10]]
 end
